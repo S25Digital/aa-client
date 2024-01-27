853123ec0f6ce1ff6dbc0240ecb3d2c0b4574dcf
Simranjeet Singh <simranjeet@s25.digital>
2024-01-27 11:09:48 +0530

    [publishing wip version]

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import { JWK } from "jose";
 import AAClient from "./client";
 import axios from "axios";
 
-export * from "../types";
-
 export function createAAClient(privateKey: JWK){
   return new AAClient({
     privateKey,
