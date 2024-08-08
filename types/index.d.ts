b3b84f884f75f5d0427c8be6dc57323763daab6d
Simranjeet Singh <simranjeet@s25.digital>
2024-08-08 19:41:59 +0530

    [fixing types]

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,4 +4,4 @@ export * as ConsentTypes from "./consent";
 export * as FITypes from "./fi";
 export * from "./common";
 
-export function createAAClient(privateKey: JWK): AAClient;
+export function createAAClient(privateKey: JWK, level?: "debug" | "error" = "error"): AAClient;
